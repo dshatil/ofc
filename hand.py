602bcdf86708002e550fc5756767e91bfeb91138
dshatil <dan.naftali.shatil@gmail.com>
2016-04-19 22:13:01 -0400

    added hand class

---
+++ hand.py
@@ -0,0 +1,15 @@
+from .card import Card
+
+class Hand(object):
+	
+	def __init__(self): # creates an empty hand
+		self._cards = []
+		
+	def __len__(self): # return number of cards
+		return len(self._cards)
+	
+	def __iter__(self):
+		return iter(self._cards)
+		
+	def add(self, card): # adds a card to the hand
+		self._cards.append(card)
