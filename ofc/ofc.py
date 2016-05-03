b947ad8857df5f4fa9fd3692bb35f2bbab931570
dshatil <dan.naftali.shatil@gmail.com>
2016-05-03 17:57:09 -0400

    update ofc.py

--- ofc/ofc.py
+++ ofc/ofc.py
@@ -24,3 +24,17 @@ class Player(object):
         self.midHand.printHand()
         print ("\n\nBottom Hand: ")
         self.botHand.printHand()
+
+    def oneCard(self, deck):
+        nextCard = deck.pop()
+        print "the current card is " + nextCard.value
+        choice = raw_input("where would you like this card?\nplease enter bot or 1 for bottom,\nmid or 2 for middle, top or 3 for top\n--> ")
+        if choice == "1" or choice == "bot":
+            self.addBot(nextCard)
+        elif choice == "2" or choice == "mid":
+            self.addMid(nextCard)
+        elif choice == "3" or choice == "top":
+            self.addTop(nextCard)
+        else:
+            print "that choice was not recognized!"
+            deck.heap(nextCard)
